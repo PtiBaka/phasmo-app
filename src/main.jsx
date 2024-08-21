ade4909e046b7fc78a3d666954d73a31003fa678
AntonyL <antony@PCPortable.localdomain>
2024-08-21 20:12:15 +0200

    ask refresh

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,27 @@
-import { StrictMode } from 'react'
+import { StrictMode, useEffect } from 'react'
 import { createRoot } from 'react-dom/client'
 import Page from './components/page'
 
+function App() {
+  useEffect(() => {
+    const handleBeforeUnload = (event) => {
+      event.preventDefault();
+      event.returnValue = ''; // C'est nécessaire pour certains navigateurs comme Chrome
+    };
+
+    window.addEventListener('beforeunload', handleBeforeUnload);
+
+    // Nettoyage lors de la destruction du composant
+    return () => {
+      window.removeEventListener('beforeunload', handleBeforeUnload);
+    };
+  }, []);
+
+  return <Page />;
+}
+
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <Page />
+    <App />
   </StrictMode>,
-)
+);
